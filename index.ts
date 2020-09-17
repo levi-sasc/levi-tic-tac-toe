e6aca490d4428988aa9c1715f71112c7fa215814
levi-sasc <levi-sasc@dataindependence.net>
2020-09-16 19:33:45 -0600

    2 files modified

--- index.ts
+++ index.ts
@@ -1,5 +1,17 @@
-// alert('Hello World!')
-var tdElements = document.getElementsByTagName("TD");
-for(var tdElement of tdElements){
+var tdElements = document.getElementsByTagName('TD');
+var isO = false;
 
+for(var tdElement of tdElements) {
+  tdElement. addEventListener('click', handleClick);
+}
+
+function handleClick(event) {
+  var target = event.target;
+  if (isO) {
+    target.innerHTML = 'O';
+    isO = false;
+  } else {
+    target.innerHTML = 'X';
+    isO = true;
+  }
 }
