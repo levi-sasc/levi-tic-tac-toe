ddf06687fc73c6a0f8ff93abb2bb0ae3ff3f936f
levi-sasc <levi-sasc@dataindependence.net>
2020-09-17 21:57:35 -0600

    3 files modified

--- index.ts
+++ index.ts
@@ -7,11 +7,41 @@ for(var tdElement of tdElements) {
 
 function handleClick(event) {
   var target = event.target;
+
+  // Prevent from changing again
+  if (target.innerHTML == 'X' || target.innerHTML == 'O') {
+    return
+  }    
+  
   if (isO) {
     target.innerHTML = 'O';
     isO = false;
   } else {
     target.innerHTML = 'X';
     isO = true;
+    check_winner('X')
+   }
   }
+
+  //get html from specific td
+  function gv(Id) {
+    var el = document.getElementById(Id)
+    return el.innerHTML
+  }
+
+function check_winner(smb) {
+
+//check winning combinations
+if ( (gv('a1') == smb &&  gv('a2') == smb &&  gv('a3')== smb) ||
+ (gv('b1') == smb &&  gv('b2') == smb &&  gv('b3') == smb) ||
+  (gv('a2') == smb &&  gv('b2') == smb &&  gv('c3') == smb) ||
+    (gv('a3') == smb &&  gv('b2') == smb &&  gv('c1') == smb) ||
+     (gv('a1') == smb &&  gv('b1') == smb &&  gv('c1') == smb) ||
+       (gv('c1') == smb &&  gv('c2') == smb &&  gv('c3') == smb)||
+          (gv('a3') == smb &&  gv('b3') == smb &&  gv('c3') == smb)||
+             (gv('a1') == smb &&  gv('b2') == smb &&  gv('c3')==smb))
+
+alert ('You are the winner')
 }
+
+//alert ('do not do anything, I am bad at programing')
